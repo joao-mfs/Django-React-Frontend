d6268100ac6f271205868a8b4504188740a1908d
joao-mfs <jmfelix83@gmail.com>
2020-10-15 15:43:30 -0300

    fixing and issue on Items

--- src/Item.js
+++ src/Item.js
@@ -2,5 +2,5 @@ import React from 'react';
 
 export default function Item(props){
     const status = props.status;
-    return <li>{props.name} <p>Status: {status ? <div>Finalizado</div> : <div>Nao Finalizado</div> }</p></li>
+    return <li>{props.name} <div>Status: {status ? <div>Finalizado</div> : <div>Nao Finalizado</div> }</div></li>
 }
